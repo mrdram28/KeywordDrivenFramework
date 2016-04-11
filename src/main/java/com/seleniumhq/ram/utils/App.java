3e6f79256474a6eb17d4b3a2cf8270ee414c664a
Ram <mrdram28@gmail.com>
2016-04-11 20:45:13 +0530

    initial commit

---
+++ src/main/java/com/seleniumhq/ram/utils/App.java
@@ -0,0 +1,13 @@
+package com.seleniumhq.ram.utils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
